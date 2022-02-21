f5f814fcdad8f7196051e5b7e5d5b656797f8a5f
Josue Garcia <yoshuah809@gmail.com>
2022-02-21 14:41:57 -0600

    added the connection to the API using fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [products, setProducts] = useState([
+		{ name: "product1", price: 100.0 },
+		{ name: "product2", price: 110.0 },
+		{ name: "product3", price: 125.0 },
+	]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/api/products")
+			.then((response) => response.json())
+			.then((data) => setProducts(data));
+	}, []);
+
+	function addProducts() {
+		setProducts((prevState) => [
+			...prevState,
+			{
+				name: "product" + (prevState.length + 1),
+				price: prevState.length * 15,
+			},
+		]);
+	}
+	return (
+		<div className="container py-3">
+			<h1 style={{ color: "blue" }}> Re-Store</h1>
+			<button onClick={addProducts}>Add Product</button>
+			<ul>
+				{products.map((p) => (
+					<li key={p.name}>
+						{" "}
+						<b>Name: </b> {p.name} Price: {p.price}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
